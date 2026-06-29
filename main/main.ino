22724321747c9d023ea96377153b71ab064f1719
Wakimae <akumakumu@gmail.com>
2026-06-29 15:23:55 +0700

    pot to adjust the delay

--- main/main.ino
+++ main/main.ino
@@ -1,9 +1,27 @@
+#include <Servo.h>
+
+Servo srv;
+
 #define ONBOARD_LED PC13
+#define SERVO_PIN PB9
+#define POT_PIN PA7
 
 void setup() {
+  srv.attach(SERVO_PIN);
   pinMode(ONBOARD_LED, OUTPUT);
+  pinMode(POT_PIN, INPUT);
 }
 
 void loop() {
   digitalWrite(ONBOARD_LED, 0);
+
+  int val = analogRead(POT_PIN);
+
+  int time_delay = map(val, 0, 1023, 1000, 100);
+
+  srv.write(0);
+  delay(time_delay);
+
+  srv.write(90);
+  delay(time_delay);
 }
